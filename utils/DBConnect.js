913238e5f2c98d0da51c187b49b965825174a058
U Ahmad <UmadAhmad1928@gmail.com>
2020-11-23 20:16:27 +0500

    added local db configuration option

--- utils/DBConnect.js
+++ utils/DBConnect.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
+   const getDBString = () => {
+      if (process.argv[2] === 'dblocal') {
+         console.log('Connecting to Local DB ...');
+         return DB_LOCAL.trim();
+      } else {
+         console.log('Connecting to Cluster DB ...');
+         return DB_URL.replace('<DB_USER>', DB_USER)
+            .replace('<DB_PASSWORD>', DB_PASSWORD)
+            .trim();
+      }
+   };
+
    const DB_URL = process.env.DB;
    const DB_PASSWORD = process.env.DB_PASSWORD;
    const DB_USER = process.env.DB_USER;
-
-   const DB = DB_URL.replace('<DB_USER>', DB_USER)
-      .replace('<DB_PASSWORD>', DB_PASSWORD)
-      .trim();
-
+   const DB_LOCAL = process.env.DB_LOCAL;
+   const DB = getDBString();
    // console.log('DB', DB);
    // console.log(DB_URL);
    //connecting to db
@@ -17,7 +26,6 @@ module.exports = () => {
          useNewUrlParser: true,
          useUnifiedTopology: true,
       })
-
       .then((res) => console.log('Database COnnected Successfully !'))
       .catch((err) => {
          console.clear();
