3e2b5b9c337c5a5c88c0b1264de8d6b2adaf8dcf
U Ahmad <UmadAhmad1928@gmail.com>
2020-12-05 16:39:23 +0500

    Refactored Palette Model

--- models/PaletteModel.js
+++ models/PaletteModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const paletteSchema = new mongoose.Schema({
    name: {
       type: String,
-      required: true,
+      required: [true, 'A Palette must have a name'],
       trim: true,
       unique: true,
    },
